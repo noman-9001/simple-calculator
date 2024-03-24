7ca2f0132b456c68ad62b393f0cef0fecf453ba1
noman-9001 <164705777+noman-9001@users.noreply.github.com>
2024-03-24 08:16:34 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Printing a Wellcome Message
+console.log("\n\tWellcome To \Nomanqureshi\ -CLI simple calculator\n");
+
+
+// Asking Questions from Users through inquirer
+
+let answers = await inquirer.prompt([
+   {message: "Enter First Number", type: "number", name: "firstnumber"},
+   {message: "Enter second Number", type: "number", name: "secondnumber"},
+   {
+        message: "select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtractions", "multiplication", "division"],
+    },
+]);
+
+// Condtional statements If-Else
+
+if(answers.operator === "addition"){
+    console.log(answers.firstnumber + answers.secondnumber)
+}
+else if(answers.operator === "subtractions"){
+    console.log(answers.firstnumber - answers.secondnumber)
+}
+else if(answers.operator === "multiplication"){
+    console.log(answers.firstnumber * answers.secondnumber)
+}
+else if(answers.operator === "division"){
+    console.log(answers.firstnumber / answers.secondnumber)
+}
+else{
+    console.log("Invalid Input")
+}
